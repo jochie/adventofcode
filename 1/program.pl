1ec930b8e22aa1ae5ddb5d6b0f7ebe3d7456ef0d
Erwin Harte <github@spam.is-here.com>
2022-12-01 19:55:10 -0800

    Day 1

---
+++ 1/program.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+my $max = -1;
+my $sum = 0;
+my $elf = 0;
+my @list = ();
+while (<>) {
+    chomp;
+    # Separator
+    if (/^$/) {
+	if ($sum > 0) {
+	    $elf++;
+	    print("Elf $elf: $sum\n");
+	    if ($sum > $max) {
+		$max = $sum;
+	    }
+	    push @list, $sum;
+	}
+	$sum = 0;
+    } else {
+	$sum += $_;
+    }
+}
+
+if ($sum > 0) {
+    $elf++;
+    print("Elf $elf: $sum\n");
+    if ($sum > $max) {
+	$max = $sum;
+    }
+    push @list, $sum;
+}
+print("Max: $max\n");
+
+@list = sort { $b <=> $a } @list;
+print(join(" ", @list).$/);
+my $top3 = $list[0] + $list[1] + $list[2];
+print("sum(Top 3) = $top3\n");
