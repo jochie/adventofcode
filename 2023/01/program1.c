d7ba432f04da936b169c78caa3a8e9397d4d8528
Erwin Harte <github@spam.is-here.com>
2023-12-01 17:43:00 -0800

    Year 2023, Day 1

---
+++ 2023/01/program1.c
@@ -0,0 +1,149 @@
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil -*- for emacs
+ *
+ * =======================================================================
+ * C code template for AoC programs
+ * =======================================================================
+ */
+
+# include <libgen.h>  /* basename()           */
+# include <stdbool.h> /* bool, true, false    */
+# include <stdio.h>   /* FILE, *printf(), etc */
+# include <stdlib.h>  /* exit()               */
+# include <string.h>  /* strlen()             */
+# include <unistd.h>  /* getopt()             */
+
+# define MAX_LEN 1024
+
+struct {
+    bool debug;
+    bool dryrun;
+    bool verbose;
+} opts;
+
+
+void parse_options(int argc, char *argv[]);
+void print_usage(FILE *f, char *argv0, char *prefix, bool full, int exitcode);
+
+
+int
+main(int argc, char *argv[], char *env[])
+{
+    parse_options(argc, argv);
+
+    char buf[MAX_LEN + 1];
+    int sum = 0;
+    while (NULL != fgets(buf, MAX_LEN, stdin)) {
+        /* Strip the newline, if present */
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = '\0';
+        }
+        if (opts.debug) {
+            printf("DEBUG: Line received: '%s'\n", buf);
+        }
+        int df = -1;
+        int dl = -1;
+        for (int i = 0; i < strlen(buf); i++) {
+            if (buf[i] >= '0' && buf[i] <= '9') {
+                int val = buf[i] - '0';
+                if (opts.debug) {
+                    printf("Digit found: %d\n", val);
+                }
+                if (df == -1) {
+                    df = val;
+                }
+                dl = val;
+            }
+        }
+        if (opts.debug) {
+            printf("First digit: %d; Last digit: %d\n", df, dl);
+        }
+        int cal = df * 10 + dl;
+        if (opts.debug) {
+            printf("Calibration value: %d\n", cal);
+        }
+        sum += cal;
+    }
+    if (opts.debug) {
+        printf("DEBUG: End of file\n");
+    }
+    printf("Sum of the calibration values: %d\n", sum);
+}
+
+
+void
+parse_options(int argc, char *argv[])
+{
+    int ch;
+
+    opts.debug   = false;
+    opts.dryrun  = false;
+    opts.verbose = false;
+
+    if (argc == 1) {
+        /* What to do if there are no command line arguments */
+        return;
+    }
+    while ((ch = getopt(argc, argv, "dnvh")) != -1) {
+        switch (ch) {
+        case 'd':
+            opts.debug = true;
+            break;
+        case 'n':
+            opts.dryrun = true;
+            break;
+        case 'v':
+            opts.verbose = true;
+            break;
+        case 'h':
+            print_usage(stdout, argv[0], NULL, true, EXIT_SUCCESS);
+        case '?':
+        default:
+            print_usage(stderr, argv[0], "\n", false, EXIT_FAILURE);
+        }
+    }
+    argc -= optind;
+    if (argc > 0) {
+        /* What to do with the non-option parameters? */
+        print_usage(stderr, argv[0], "Too many parameters provided.\n\n", false, EXIT_FAILURE);
+    }
+    argv += optind;
+}
+
+
+void
+print_usage(FILE *f, char *argv0, char *prefix, bool full, int exitcode)
+{
+    char *name;
+
+    if (prefix != NULL) {
+        fprintf(f, "%s", prefix);
+    }
+    name = basename(argv0);
+    fprintf(f, "\
+NAME\n\
+     %s - Program for AoC 2023 puzzles; Day 1, part 1\n\
+\n\
+SYNOPSIS\n\
+     %s [OPTIONS]\n",
+            name, name);
+    if (!full) {
+        exit(exitcode);
+    }
+    fprintf(f, "\
+\n\
+DESCRIPTION\n\
+     This program is used for one of the AoC 2023 puzzles; Day 1, part 1.\n\
+\n\
+OPTIONS\n\
+     -d\n\
+        Enable debugging output.\n\
+     -n\n\
+        Request dryrun (noop) mode.\n\
+     -v\n\
+        Enable verbose output.\n\
+\n\
+EXIT STATUS\n\
+     The program exits 0 on success, and 1 when something went amiss\n\
+     with its option parsing.\n");
+    exit(exitcode);
+}
