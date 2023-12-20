20b13f6880fe29fe4a4ba0dfe90636032ff96771
Erwin Harte <github@spam.is-here.com>
2023-12-19 18:52:38 -0800

    Year 2023, Day 19
    Part 2 felt like a throwback to Day 5.

--- 2023/19/program1.c
+++ 2023/19/program1.c
@@ -16,9 +16,9 @@
 
 # include <openssl/sha.h>
 
-# define YEAR YYYY
-# define DAY    DD
-# define PART    Z
+# define YEAR 2023
+# define DAY    19
+# define PART    1
 
 # define STR(x) _STR(x)
 # define _STR(x) #x
@@ -79,6 +79,95 @@ main(int argc, char *argv[], char *env[])
     }
 }
 
+typedef enum names {
+    NAME_X,
+    NAME_M,
+    NAME_A,
+    NAME_S
+} name;
+
+typedef struct rule {
+    name attr;
+    char op;
+    int  value;
+    char target[5];
+} rule;
+
+typedef struct workflow {
+    char name[5];
+    rule rules[5];
+    int  count;
+    char fallback[5];
+} workflow;
+
+workflow workflows[600];
+
+int count = 0;
+
+
+bool
+process_workflows(int flow, int attr[4])
+{
+    if (opts.debug) {
+        printf("process_workflows(%d, %d, %d, %d)\n",
+               attr[0], attr[1], attr[2], attr[3]);
+    }
+    while (true) {
+        char *next = NULL;
+
+        if (opts.debug) {
+            printf("Checking against workflow %d\n", flow);
+        }
+        for (int r = 0; r < workflows[flow].count; r++) {
+            if (opts.debug) {
+                printf("  Checking against rule %d: %d %c %d (attr[%d] = %d)\n",
+                       r,
+                       workflows[flow].rules[r].attr,
+                       workflows[flow].rules[r].op,
+                       workflows[flow].rules[r].value,
+                       workflows[flow].rules[r].attr,
+                       attr[workflows[flow].rules[r].attr]);
+            }
+            switch (workflows[flow].rules[r].op) {
+            case '<':
+                if (attr[workflows[flow].rules[r].attr] < workflows[flow].rules[r].value) {
+                    next = workflows[flow].rules[r].target;
+                }
+                break;
+            case '>':
+                if (attr[workflows[flow].rules[r].attr] > workflows[flow].rules[r].value) {
+                    next = workflows[flow].rules[r].target;
+                }
+                break;
+            default:
+                printf("What operator is this? %c\n", workflows[flow].rules[r].op);
+                exit(1);
+            }
+            if (NULL != next) {
+                break; /* for */
+            }
+        }
+        if (NULL == next) {
+            next = workflows[flow].fallback;
+        }
+        if (!strcmp(next, "A")) {
+            return true;
+        }
+        if (!strcmp(next, "R")) {
+            return false;
+        }
+        for (int w = 0; w < count; w++) {
+            if (!strcmp(workflows[w].name, next)) {
+                if (opts.debug) {
+                    printf("Moving on to %s (%d)\n", next, w);
+                }
+                flow = w;
+                break;
+            }
+        }
+    }
+    return false;
+}
 
 /*
  * Read from the filedescriptor (whether it's stdin or an actual file)
@@ -89,7 +178,11 @@ void
 process_file(FILE *fd)
 {
     char buf[MAX_LEN + 1];
+    int total = 0;
 
+    count = 0;
+
+    /* Parse workflows */
     while (NULL != fgets(buf, MAX_LEN, fd)) {
         /* Strip the newline, if present */
         if (buf[strlen(buf) - 1] == '\n') {
@@ -107,10 +200,100 @@ process_file(FILE *fd)
 
             printf("DEBUG: Line received: [%s] '%s'\n", hexdigest, buf);
         }
+        if (!strlen(buf)) {
+            break;
+        }
+        char name[5];
+        char info[MAX_LEN + 1];
+        sscanf(buf, "%[a-z]{%[^}]}", name, info);
+
+        strcpy(workflows[count].name, name);
+        workflows[count].count = 0;
+
+        int total = 0;
+        char *p = info;
+        while (true) {
+            char *sep = strchr(p, ',');
+            if (NULL == sep) {
+                if (opts.debug) {
+                    printf("  Fallback: %s\n", p);
+                }
+                strcpy(workflows[count].fallback, p);
+                break;
+            }
+            sep[0] = '\0';
+            char attr, op;
+            int value;
+            char target[5];
+            sscanf(p, "%c%c%d:%s", &attr, &op, &value, target);
+            switch (attr) {
+            case 'x': workflows[count].rules[total].attr = NAME_X; break;
+            case 'm': workflows[count].rules[total].attr = NAME_M; break;
+            case 'a': workflows[count].rules[total].attr = NAME_A; break;
+            case 's': workflows[count].rules[total].attr = NAME_S; break;
+            }
+            workflows[count].rules[total].op = op;
+            workflows[count].rules[total].value = value;
+            strcpy(workflows[count].rules[total].target, target);
+            if (opts.debug) {
+                printf("  Rule: %c %c %d => %s\n", attr, op, value, target);
+            }
+            total++;
+            p = sep + 1;
+        }
+        workflows[count].count = total;
+        if (opts.debug) {
+            printf("Total of %d rules\n", total);
+        }
+        count++;
+    }
+    if (opts.debug) {
+        printf("Found a total of %d workflows\n", count);
+    }
+
+    int flow_start = 0;
+    for (int w = 0; w < count; w++) {
+        if (!strcmp(workflows[w].name, "in")) {
+            flow_start = w;
+            break;
+        }
+    }
+    if (opts.debug) {
+        printf("Workflow to start at: %d\n", flow_start);
+        printf("\n");
+    }
+    while (NULL != fgets(buf, MAX_LEN, fd)) {
+        /* Strip the newline, if present */
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = '\0';
+        }
+        if (opts.debug) {
+            unsigned char digest[SHA256_DIGEST_LENGTH];
+            char hexdigest[SHA256_DIGEST_LENGTH * 2 + 1];
+
+            SHA256((unsigned char *)buf, strlen(buf), digest);
+
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+                sprintf(&hexdigest[i * 2], "%02x", (unsigned int)digest[i]);
+            }
+
+            printf("DEBUG: Line received: [%s] '%s'\n", hexdigest, buf);
+        }
+        int attr[4];
+        sscanf(buf, "{x=%d,m=%d,a=%d,s=%d}", &attr[0], &attr[1], &attr[2], &attr[3]);
+
+        if (process_workflows(flow_start, attr)) {
+            int sum = attr[0] + attr[1] + attr[2] + attr[3];
+            if (opts.debug) {
+                printf("  Accepted item: %d\n", sum);
+            }
+            total += sum;
+        }
     }
     if (opts.debug) {
         printf("DEBUG: End of file\n");
     }
+    printf("Total accepted: %d\n", total);
 }
 
 
--- 2023/19/program2.c
+++ 2023/19/program2.c
@@ -16,9 +16,9 @@
 
 # include <openssl/sha.h>
 
-# define YEAR YYYY
-# define DAY    DD
-# define PART    Z
+# define YEAR 2023
+# define DAY    19
+# define PART    2
 
 # define STR(x) _STR(x)
 # define _STR(x) #x
@@ -79,6 +79,254 @@ main(int argc, char *argv[], char *env[])
     }
 }
 
+typedef enum names {
+    NAME_X,
+    NAME_M,
+    NAME_A,
+    NAME_S
+} name;
+
+typedef struct rule {
+    name attr;
+    char op;
+    int  value;
+    char target[5];
+} rule;
+
+typedef struct workflow {
+    char name[5];
+    rule rules[5];
+    int  count;
+    char fallback[5];
+} workflow;
+
+workflow workflows[600];
+
+int count = 0;
+
+typedef struct segment {
+    int    flow;
+    int    ranges[4][2];
+} segment;
+
+segment segments[400];
+int segments_total = 0;
+
+
+void
+dump_ranges(int ranges[4][2])
+{
+    for (int i = 0; i < 4; i++) {
+        printf("  Attribute %d: %4d - %4d\n", i, ranges[i][0], ranges[i][1]);
+    }
+}
+
+void
+add_segment(int flow, int ranges[4][2])
+{
+    segments[segments_total].flow = flow;
+    for (int i = 0; i < 4; i++) {
+        segments[segments_total].ranges[i][0] = ranges[i][0];
+        segments[segments_total].ranges[i][1] = ranges[i][1];
+    }
+    if (opts.debug) {
+        printf("Adding segment #%d at workflow %d:\n", segments_total + 1, flow);
+        dump_ranges(segments[segments_total].ranges);
+    }
+    segments_total++;
+}
+
+
+long combinations = 0;
+
+# define WORKFLOW_A -1
+# define WORKFLOW_R -2
+# define WORKFLOW_E -3
+
+int
+find_workflow(char name[5])
+{
+    if (!strcmp(name, "A")) {
+        return WORKFLOW_A;
+    }
+    if (!strcmp(name, "R")) {
+        return WORKFLOW_R;
+    }
+    for (int w = 0; w < count; w++) {
+        if (!strcmp(workflows[w].name, name)) {
+            return w;
+        }
+    }
+    return WORKFLOW_E;
+}
+
+void
+add_combinations(int ranges[4][2])
+{
+    long comb = 1;
+
+    for (int i = 0; i < 4; i++) {
+        comb = comb * (ranges[i][1] - ranges[i][0] + 1);
+    }
+    if (opts.debug) {
+        printf("Adding %ld combinations to the pile.\n", comb);
+    }
+    combinations += comb;
+}
+
+void
+process_segments()
+{
+    while (segments_total > 0) {
+        /* pop the last one off of the stack */
+        segment cur;
+
+        memcpy(&cur, &segments[segments_total - 1], sizeof(segment));
+        segments_total--;
+
+        if (opts.debug) {
+            printf("Segment at workflow %d\n", cur.flow);
+            dump_ranges(cur.ranges);
+        }
+
+        int flow = cur.flow;
+        char *next = NULL;
+
+        bool done = false;
+        if (opts.debug) {
+            printf("Checking against workflow %d\n", flow);
+        }
+        for (int r = 0; r < workflows[flow].count; r++) {
+            int attr = workflows[flow].rules[r].attr;
+            bool split = false;
+            int ranges_copy[4][2];
+
+            for (int i = 0; i < 4; i++) {
+                ranges_copy[i][0] = cur.ranges[i][0];
+                ranges_copy[i][1] = cur.ranges[i][1];
+            }
+
+            if (opts.debug) {
+                printf("  Checking against rule %d: %d %c %d (attr range = %d - %d)\n",
+                       r,
+                       attr,
+                       workflows[flow].rules[r].op,
+                       workflows[flow].rules[r].value,
+                       cur.ranges[attr][0], cur.ranges[attr][1]);
+            }
+
+            switch (workflows[flow].rules[r].op) {
+            case '<':
+                if (cur.ranges[attr][1] < workflows[flow].rules[r].value) {
+                    /*
+                     * Completely to the left
+                     *                              < [value]
+                     * [0]==== ranges[attr] ====[1]
+                     */
+                    next = workflows[flow].rules[r].target;
+                    done = true;
+                } else if (cur.ranges[attr][0] < workflows[flow].rules[r].value) {
+                    /*
+                     * Partially to the left
+                     *
+                     *  <cur>    < [value]   <copy>
+                     * [0]==== ranges[attr] ====[1]
+                     */
+                    split = true;
+                    next = workflows[flow].rules[r].target;
+
+                    /* cur.ranges keeps searching for a match */
+                    cur.ranges[attr][0] = workflows[flow].rules[r].value;
+
+                    /* copy is going to be processed below */
+                    ranges_copy[attr][1] = workflows[flow].rules[r].value - 1;
+                }
+                break;
+            case '>':
+                if (cur.ranges[attr][0] > workflows[flow].rules[r].value) {
+                    /*
+                     * Completely to the right
+                     *
+                     * [value] >
+                     *           [0]==== ranges[attr] ====[1]
+                     */
+                    next = workflows[flow].rules[r].target;
+                    done = true;
+                } else if (cur.ranges[attr][1] > workflows[flow].rules[r].value) {
+                    /*
+                     * Partially to the right
+                     *
+                     * <copy>   [value] >     <cur>
+                     * [0]==== ranges[attr] ====[1]
+                     */
+                    split = true;
+                    next = workflows[flow].rules[r].target;
+
+                    /* cur.ranges keeps searching for a match */
+                    cur.ranges[attr][1] = workflows[flow].rules[r].value;
+
+                    /* copy is going to be processed below */
+                    ranges_copy[attr][0] = workflows[flow].rules[r].value + 1;
+                }
+                break;
+            default:
+                printf("What operator is this? %c\n", workflows[flow].rules[r].op);
+                exit(1);
+            }
+            if (NULL != next) {
+                int newflow = find_workflow(next);
+                switch (newflow) {
+                case WORKFLOW_A:
+                    if (opts.debug) {
+                        printf("Done with this segment:\n");
+                        dump_ranges(ranges_copy);
+                    }
+                    add_combinations(ranges_copy);
+                    break;
+                case WORKFLOW_R:
+                    break;
+                case WORKFLOW_E:
+                    printf("Could not find the workflow for %s?\n", next);
+                    exit(1);
+                default:
+                    add_segment(newflow, ranges_copy);
+                    break;
+                }
+                if (!split) {
+                    break; /* for */
+                }
+            }
+        }
+
+        if (done) {
+            continue;
+        }
+
+        next = workflows[flow].fallback;
+        int newflow = find_workflow(next);
+        switch (newflow) {
+        case WORKFLOW_A:
+            if (opts.debug) {
+                printf("Segment ACCEPTED:\n");
+                dump_ranges(cur.ranges);
+            }
+            add_combinations(cur.ranges);
+            break;
+        case WORKFLOW_R:
+            if (opts.debug) {
+                printf("Segment REJECTED:\n");
+                dump_ranges(cur.ranges);
+            }
+            break;
+        case WORKFLOW_E:
+            printf("Could not find the workflow for %s?\n", next);
+            exit(1);
+        default:
+            add_segment(newflow, cur.ranges);
+            break;
+        }
+    }
+}
 
 /*
  * Read from the filedescriptor (whether it's stdin or an actual file)
@@ -89,7 +337,12 @@ void
 process_file(FILE *fd)
 {
     char buf[MAX_LEN + 1];
+    int total = 0;
 
+    count = 0;
+    combinations = 0;
+
+    /* Parse workflows */
     while (NULL != fgets(buf, MAX_LEN, fd)) {
         /* Strip the newline, if present */
         if (buf[strlen(buf) - 1] == '\n') {
@@ -107,10 +360,84 @@ process_file(FILE *fd)
 
             printf("DEBUG: Line received: [%s] '%s'\n", hexdigest, buf);
         }
+        if (!strlen(buf)) {
+            break;
+        }
+        if (opts.debug) {
+            printf("A: %s\n", buf);
+        }
+        char name[5];
+        char info[MAX_LEN + 1];
+        sscanf(buf, "%[a-z]{%[^}]}", name, info);
+
+        strcpy(workflows[count].name, name);
+        workflows[count].count = 0;
+
+        int total = 0;
+        char *p = info;
+        while (true) {
+            char *sep = strchr(p, ',');
+            if (NULL == sep) {
+                if (opts.debug) {
+                    printf("  Fallback: %s\n", p);
+                }
+                strcpy(workflows[count].fallback, p);
+                break;
+            }
+            sep[0] = '\0';
+            char attr, op;
+            int value;
+            char target[5];
+            sscanf(p, "%c%c%d:%s", &attr, &op, &value, target);
+            switch (attr) {
+            case 'x': workflows[count].rules[total].attr = NAME_X; break;
+            case 'm': workflows[count].rules[total].attr = NAME_M; break;
+            case 'a': workflows[count].rules[total].attr = NAME_A; break;
+            case 's': workflows[count].rules[total].attr = NAME_S; break;
+            }
+            workflows[count].rules[total].op = op;
+            workflows[count].rules[total].value = value;
+            strcpy(workflows[count].rules[total].target, target);
+            if (opts.debug) {
+                printf("  Rule: %c %c %d => %s\n", attr, op, value, target);
+            }
+            total++;
+            p = sep + 1;
+        }
+        workflows[count].count = total;
+        if (opts.debug) {
+            printf("Total of %d rules\n", total);
+        }
+        count++;
     }
     if (opts.debug) {
         printf("DEBUG: End of file\n");
     }
+
+    if (opts.debug) {
+        printf("Found a total of %d workflows\n", count);
+    }
+
+    int flow_start = 0;
+    for (int w = 0; w < count; w++) {
+        if (!strcmp(workflows[w].name, "in")) {
+            flow_start = w;
+            break;
+        }
+    }
+    if (opts.debug) {
+        printf("Workflow to start at: %d\n", flow_start);
+        printf("\n");
+    }
+    segments[segments_total].flow = flow_start;
+    for (int i = 0; i < 4; i++) {
+        segments[segments_total].ranges[i][0] = 1;
+        segments[segments_total].ranges[i][1] = 4000;
+    }
+    segments_total++;
+
+    process_segments();
+    printf("Total accepted: %ld\n", combinations);
 }
 
 
