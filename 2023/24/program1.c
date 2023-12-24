285e906c08a76cb72c0b69979337f78807d9f348
Erwin Harte <github@spam.is-here.com>
2023-12-24 13:59:17 -0800

    Year 2023, Day 24, part 1

--- 2023/24/program1.c
+++ 2023/24/program1.c
@@ -16,9 +16,9 @@
 
 # include <openssl/sha.h>
 
-# define YEAR YYYY
-# define DAY    DD
-# define PART    Z
+# define YEAR 2023
+# define DAY    24
+# define PART    1
 
 # define STR(x) _STR(x)
 # define _STR(x) #x
@@ -80,6 +80,116 @@ main(int argc, char *argv[], char *env[])
 }
 
 
+struct hailstone {
+    long px, py, pz;
+    long vx, vy, vz;
+} hailstones[400];
+int t_hailstones;
+
+
+bool
+check_paths(int h1, int h2, long xy_min, long xy_max)
+{
+    /*
+     *  / px1 \     / vx1 \         / px2 \     / vx2 \
+     * (  py1  ) + (  vy1  ) * s = (  py2  ) + (  vy2  ) * t
+     *  \ pz1 /     \ vz1 /         \ pz2 /     \ vz2 /
+     *
+     *    px1 + vx1 * s = px2 + vx2 * t
+     *    py1 + vy1 * s = py2 + vy2 * t
+     * =>
+     *    vx1 * s = px2 + vx2 * t - px1
+     *    vy1 * s = py2 + vy2 * t - py1
+     * =>
+     *    s = (px2 + vx2 * t - px1) / vx1
+     *    s = (py2 + vy2 * t - py1) / vy1
+     * =>
+     *    (px2 + vx2 * t - px1) / vx1 = (py2 + vy2 * t - py1) / vy1
+     * =>
+     *    (px2 + vx2 * t - px1) * vy1 = (py2 + vy2 * t - py1) * vx1
+     * =>
+     *    px2 * vy1 + vx2 * t * vy1 - px1 * vy1 = py2 * vx1 + vy2 * t * vx1 - py1 * vx1
+     * =>
+     *    vx2 * t * vy1 - vy2 * t * vx1 = py2 * vx1 - py1 * vx1 - px2 * vy1 + px1 * vy1
+     * =>
+     *    t * (vx2 * vy1 - vy2 * vx1) = py2 * vx1 - py1 * vx1 - px2 * vy1 + px1 * vy1
+     * =>
+     *    t = (py2 * vx1 - py1 * vx1 - px2 * vy1 + px1 * vy1) / (vx2 * vy1 - vy2 * vx1)
+     *
+     * =====
+     *
+     *    px1 + vx1 * s = px2 + vx2 * t
+     * =>
+     *    vx1 * s = px2 + vx2 * t - px1
+     * =>
+     *    s = (px2 + vx2 * t - px1) / vx1
+     */
+
+    double px1 = (double)hailstones[h1].px;
+    double py1 = (double)hailstones[h1].py;
+    double px2 = (double)hailstones[h2].px;
+    double py2 = (double)hailstones[h2].py;
+    double vx1 = (double)hailstones[h1].vx;
+    double vy1 = (double)hailstones[h1].vy;
+    double vx2 = (double)hailstones[h2].vx;
+    double vy2 = (double)hailstones[h2].vy;
+
+    double div = (vx2 * vy1 - vy2 * vx1);
+    if (div == 0) {
+        if (opts.debug) {
+            printf("Storms %d and %d never intersect.\n", h1 + 1, h2 + 1);
+        }
+        return false;
+    }
+    double t = (py2 * vx1 - py1 * vx1 - px2 * vy1 + px1 * vy1) / div;
+    if (opts.debug) {
+        printf("t = %f\n", t);
+    }
+    double x2 = px2 + vx2 * t;
+    double y2 = py2 + vy2 * t;
+    if (opts.debug) {
+        printf("(X2,Y2) = (%f,%f)\n", x2, y2);
+    }
+
+    double s = (px2 + vx2 * t - px1) / vx1;
+    if (opts.debug) {
+        printf("s = %f\n", s);
+    }
+    double x1 = px1 + vx1 * s;
+    double y1 = py1 + vy1 * s;
+    if (opts.debug) {
+        printf("(X1,Y1) = (%f,%f)\n", x1, y1);
+    }
+
+    if (t < 0 && s < 0) {
+        if (opts.debug) {
+            printf("They crossed, but in the past for both.\n");
+        }
+        return false;
+    }
+    if (t < 0) {
+        if (opts.debug) {
+            printf("They crossed, but it was in the past for storm %d.\n", h2 + 1);
+        }
+        return false;
+    }
+    if (s < 0) {
+        if (opts.debug) {
+            printf("They crossed, but it was in the past for storm %d.\n", h1 + 1);
+        }
+        return false;
+    }
+    if ((double)xy_min <= x1 && x1 <= (double)xy_max &&
+        (double)xy_min <= y1 && y1 <= (double)xy_max) {
+        if (opts.debug) {
+            printf("(%f,%f) is in the right grid.\n", x1, y1);
+        }
+        return true;
+    }
+    return false;
+}
+
+
 /*
  * Read from the filedescriptor (whether it's stdin or an actual file)
  * until we reach the end. Strip newlines, and then do what needs to
@@ -90,6 +200,7 @@ process_file(FILE *fd)
 {
     char buf[MAX_LEN + 1];
 
+    t_hailstones = 0;
     while (NULL != fgets(buf, MAX_LEN, fd)) {
         /* Strip the newline, if present */
         if (buf[strlen(buf) - 1] == '\n') {
@@ -107,10 +218,49 @@ process_file(FILE *fd)
 
             printf("DEBUG: Line received: [%s] '%s'\n", hexdigest, buf);
         }
+        long x1, x2, x3, x4, x5, x6;
+        if (sscanf(buf, "%ld, %ld, %ld @ %ld, %ld, %ld",
+                   &x1, &x2, &x3, &x4, &x5, &x6) == 6) {
+            hailstones[t_hailstones].px = x1;
+            hailstones[t_hailstones].py = x2;
+            hailstones[t_hailstones].pz = x3;
+            hailstones[t_hailstones].vx = x4;
+            hailstones[t_hailstones].vy = x5;
+            hailstones[t_hailstones].vz = x6;
+            t_hailstones++;
+        } else {
+            printf("Failed to parse input line: %s\n", buf);
+            exit(1);
+        }
     }
     if (opts.debug) {
         printf("DEBUG: End of file\n");
     }
+    if (opts.debug) {
+        printf("Parsed %d hailstones from the input.\n", t_hailstones);
+    }
+    long xy_min = 7;
+    long xy_max = 27;
+    if (t_hailstones > 5) {
+        /* Assume it's the real input file */
+        xy_min = 200000000000000;
+        xy_max = 400000000000000;
+    }
+    int crossed = 0;
+    if (opts.debug) {
+        printf("Looking for paths crossing for (x,y) between [%ld,%ld]\n", xy_min, xy_max);
+    }
+    for (int s = 0; s < t_hailstones; s++) {
+        for (int t = s + 1; t < t_hailstones; t++) {
+            if (opts.debug) {
+                printf("Comparing storm %d and %d\n", s + 1, t + 1);
+            }
+            if (check_paths(s, t, xy_min, xy_max)) {
+                crossed++;
+            }
+        }
+    }
+    printf("%d of the paths will cross in the test range.\n", crossed);
 }
 
 
