b60d14c70e04f12408c5f5be6973d36b81026545
Erwin Harte <github@spam.is-here.com>
2024-12-08 16:40:37 -0800

    Rewrote Day 8's code using a minimalist Vector / grid-position class

--- 2024/08/p1.py
+++ 2024/08/p1.py
@@ -31,8 +31,28 @@ def parse_options():
     return parser.parse_args()
 
 
-def on_grid(row, col, max_row, max_col):
-    return row >= 0 and row < max_row and col >= 0 and col < max_col
+# Very basic vector / grid-position class, just enough to solve this
+# puzzle.
+class Vector:
+    def __init__(self, r, c):
+        self.row = r
+        self.col = c
+
+    def __add__(self, o):
+        return Vector(self.row + o.row, self.col + o.col)
+
+    def __sub__(self, o):
+        return Vector(self.row - o.row, self.col - o.col)
+
+    def __repr__(self):
+        return f"({self.row},{self.col})"
+
+    def __eq__(self, o):
+        return self.row == o.row and self.col == o.col
+
+
+def on_grid(pos, max_row, max_col):
+    return pos.row >= 0 and pos.row < max_row and pos.col >= 0 and pos.col < max_col
 
 
 def main():
@@ -40,9 +60,9 @@ def main():
     Main section, where we parse the command line options, read the
     stdin content, and act on it
     """
+    opts = parse_options()
     antennas = {}
     grid = []
-    opts = parse_options()
     max_row = 0
     max_col = 0
     for line in sys.stdin:
@@ -57,7 +77,7 @@ def main():
                 continue
             if char not in antennas:
                 antennas[char] = []
-            antennas[char].append([ max_row, col ])
+            antennas[char].append(Vector(max_row, col))
         max_row += 1
 
         if opts.debug:
@@ -67,28 +87,24 @@ def main():
     antinodes = {}
     seen = {}
     for freq, positions in antennas.items():
-        for row1, col1 in positions:
-            for row2, col2 in positions:
-                if row1 == row2 and col1 == col2:
+        for pos1 in positions:
+            for pos2 in positions:
+                if pos1 == pos2:
                     # We're looking in the mirror
                     continue
-                if f"{row2},{col2},{row1},{col1}" in seen:
+                if f"{pos2},{pos1}" in seen:
                     # We already processed this pair from the opposite direction
                     continue
-                seen[f"{row1},{col1},{row2},{col2}"] = True
+                seen[f"{pos1},{pos2}"] = True
 
-                row_diff = row2 - row1
-                col_diff = col2 - col1
+                pos_diff = pos2 - pos1
+                anti1 = pos1 - pos_diff
+                if on_grid(anti1, max_row, max_col):
+                    antinodes[str(anti1)] = True
 
-                anti1_row = row1 - row_diff
-                anti1_col = col1 - col_diff
-                if on_grid(anti1_row, anti1_col, max_row, max_col):
-                    antinodes[f"({anti1_row},{anti1_col})"] = True
-
-                anti2_row = row2 + row_diff
-                anti2_col = col2 + col_diff
-                if on_grid(anti2_row, anti2_col, max_row, max_col):
-                    antinodes[f"({anti2_row},{anti2_col})"] = True
+                anti2 = pos2 + pos_diff
+                if on_grid(anti2, max_row, max_col):
+                    antinodes[str(anti2)] = True
 
     # Result:
     print(f"Unique antinodes found: {len(antinodes)}")
