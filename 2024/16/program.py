4e9b70033f0d219bbae7111cbd8ecd62cad26062
Erwin Harte <github@spam.is-here.com>
2024-12-22 14:50:08 -0800

    Started using a -t/--test to make refactoring easier
    Also refactored the input/file parsing code

--- 2024/16/program.py
+++ 2024/16/program.py
@@ -7,7 +7,7 @@ Python code template for AoC programs
 import argparse
 import re
 import sys
-
+import time
 
 def parse_options():
     """
@@ -30,12 +30,25 @@ def parse_options():
                         action='store_true')
     parser.add_argument('-p', '--part',
                         help="Which part to run",
-                        type=int,
-                        required=True)
+                        type=int)
     parser.add_argument('-f', '--filename',
-                        help="The filename to read (sample, input)",
-                        required=True)
-    return parser.parse_args()
+                        help="The filename to read (sample, input)")
+    parser.add_argument('-t', '--test',
+                        help="Run the program with all known files, and all parts unless one is specified.",
+                        action='store_true')
+
+    opts = parser.parse_args()
+
+    if not opts.test:
+        if not opts.part or not opts.filename:
+            print("The --part and --filename options are required unless you use --test.")
+            sys.exit(1)
+
+    if opts.test and opts.filename:
+        print("The --test and --filename options are mutually exclusive.")
+        sys.exit(1)
+
+    return opts
 
 
 DIRS = [
@@ -145,12 +158,7 @@ def run_part2(opts, grid, max_row, max_col, start_row, start_col, end_row, end_c
     return best_positions
 
 
-def main():
-    """
-    Main section, where we parse the command line options, read the
-    stdin content, and act on it
-    """
-    opts = parse_options()
+def parse_file(opts, filename):
     grid = []
     max_row = 0
     max_col = 0
@@ -159,7 +167,7 @@ def main():
     end_row = 0
     end_col = 0
     try:
-        with open(opts.filename, "r", encoding="utf-8") as input_fd:
+        with open(filename, "r", encoding="utf-8") as input_fd:
             for line in input_fd:
                 line = line.rstrip()
                 if opts.debug:
@@ -176,35 +184,72 @@ def main():
                         end_col = col
                 max_row += 1
     except FileNotFoundError:
-        print(f"ERROR: File not found {opts.filename}")
+        print(f"ERROR: File not found {filename}")
         sys.exit(1)
-    # Done reading
+    return grid, max_row, max_col, start_row, start_col, end_row, end_col
+
+
+def run_part(opts, run, part, filename, params):
+    success = False
+    time_before = time.process_time()
+    answer = run[part]["call"](opts, *params)
+    time_diff = time.process_time() - time_before
+    if filename in run[part]["files"]:
+        if answer == run[part]["files"][filename]:
+            if opts.verbose:
+                print(f"Confirmed expected value from part {part}, filename '{filename}'")
+            success = True
+        else:
+            print(f"Warning: Unexpected value from part {part}, filename '{filename}'")
+    else:
+        print(f"Warning: No known answer for part {part}, filename '{filename}'")
+    print(f"[Duration {time_diff:.2f}s] Part {part}, filename '{filename}', answer: {answer}")
+    return success
+
+
+def main():
+    """
+    Main section, where we parse the command line options, read the
+    stdin content, and act on it
+    """
+    opts = parse_options()
 
     # Function to call, expected values, for sanity-checking (sample)
     # and later factoring (both):
     run = {
         1: { "call": run_part1,
-             "sample1":  7036,
-             "sample2": 11048,
-             "input":   75416
+             "files": {
+                 "sample1":  7036,
+                 "sample2": 11048,
+                 "input":   75416
+             }
             },
         2: { "call":   run_part2,
-             "sample1":  45,
-             "sample2":  64,
-             "input":   476
+             "files": {
+                 "sample1":  45,
+                 "sample2":  64,
+                 "input":   476
+             }
             }
     }
 
-    answer = run[opts.part]["call"](opts, grid, max_row, max_col, start_row, start_col, end_row, end_col)
-    if opts.filename in run[opts.part]:
-        if answer == run[opts.part][opts.filename]:
-            if opts.verbose:
-                print(f"Confirmed expected value from part {opts.part}, filename '{opts.filename}'")
-        else:
-            print(f"Warning: Unexpected value from part {opts.part}, filename '{opts.filename}'")
-    else:
-        print(f"Warning: No known answer for part {opts.part}, filename '{opts.filename}'")
-    print(f"Answer: {answer}")
+    if not opts.test:
+        params = parse_file(opts, opts.filename)
+        run_part(opts, run, opts.part, opts.filename, params)
+        return 0
+    passed = 0
+    failed = 0
+    for part in run.keys():
+        if opts.part and opts.part != part:
+            continue
+        for filename in run[part]["files"].keys():
+            params = parse_file(opts, filename)
+            if run_part(opts, run, part, filename, params):
+                passed += 1
+            else:
+                failed += 1
+    print(f"Test results: {passed} passed, {failed} failed.")
+    return 0
 
 
 if __name__ == "__main__":
