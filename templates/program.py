0f3e74fa466dbbbb15c4d3a0b3658672b5541f47
Erwin Harte <github@spam.is-here.com>
2024-12-22 15:35:25 -0800

    Improvements to the Python template
    - Combine part 1 and 2 into a single program
    - Add -f/--filename argument instead of reading from stdin
    - Moved input reading/parsing code into separate function, for clarity
    - Add -t/--test for (regression) testing the solution against
      the (sometimes multiple) samples, and the real input
    - Measure the duration of each attempt

--- templates/program.py
+++ templates/program.py
@@ -7,13 +7,15 @@ Python code template for AoC programs
 import argparse
 import re
 import sys
+import time
+
 
 def parse_options():
     """
     Parser command line options
     """
     parser = argparse.ArgumentParser(
-        description='This program is used for one of the AoC YYYY puzzles; Day X, part Z'
+        description='This program is used for one of the AoC YYYY puzzles; Day X'
     )
     parser.add_argument('-d', '--debug',
                         help="Enable debug output",
@@ -27,7 +29,79 @@ def parse_options():
                         help="Enable verbose output",
                         default=False,
                         action='store_true')
-    return parser.parse_args()
+    parser.add_argument('-p', '--part',
+                        help="Which part to run",
+                        type=int)
+    parser.add_argument('-f', '--filename',
+                        help="The filename to read (sample, input)")
+    parser.add_argument('-t', '--test',
+                        help="Run the program with all known files, and all parts unless one is specified.",
+                        action='store_true')
+
+    opts = parser.parse_args()
+
+    if not opts.test:
+        if not opts.part or not opts.filename:
+            print("The --part and --filename options are required unless you use --test.")
+            sys.exit(1)
+
+    if opts.test and opts.filename:
+        print("The --test and --filename options are mutually exclusive.")
+        sys.exit(1)
+
+    return opts
+
+
+def run_part1(opts, lines):
+    # Placeholder return value
+    if opts.debug:
+        print(opts)
+        print(lines)
+
+    return -1
+
+
+def run_part2(opts, lines):
+    # Placeholder return value
+    if opts.debug:
+        print(opts)
+        print(lines)
+
+    return -2
+
+
+def parse_file(opts, filename):
+    lines = []
+    try:
+        with open(filename, "r", encoding="utf-8") as input_fd:
+            for line in input_fd:
+                line = line.rstrip()
+                if opts.debug:
+                    print(f"DEBUG: Line received: '{line}'")
+                lines.append(line)
+    except FileNotFoundError:
+        print(f"ERROR: File not found {filename}")
+        sys.exit(1)
+    return lines,
+
+
+def run_part(opts, run, part, filename, params):
+    success = False
+    time_before = time.process_time()
+    answer = run[part]["call"](opts, *params)
+    time_diff = time.process_time() - time_before
+    if filename in run[part]["files"]:
+        if answer == run[part]["files"][filename]:
+            if opts.verbose:
+                print(f"Confirmed expected value from part {part}, filename '{filename}'")
+            success = True
+        else:
+            print(f"Warning: Unexpected value from part {part}, filename '{filename}'")
+    else:
+        print(f"Warning: No known answer for part {part}, filename '{filename}'")
+    print(f"[Duration {time_diff:.2f}s] Part {part}, filename '{filename}', answer: {answer}")
+    return success
+
 
 def main():
     """
@@ -35,11 +109,43 @@ def main():
     stdin content, and act on it
     """
     opts = parse_options()
-    for line in sys.stdin:
-        line = line.rstrip()
-        if opts.debug:
-            print(f"DEBUG: Line received: '{line}'")
-    # Done
+
+    # Function to call, expected values, for sanity-checking (sample)
+    # and later factoring (both):
+    run = {
+        1: {
+            "call": run_part1,
+            "files": {
+                "sample": 0,
+                "input":  0
+            }
+        },
+        2: {
+            "call": run_part2,
+            "files": {
+                "sample": 0,
+                "input":  0
+            }
+        }
+    }
+
+    if not opts.test:
+        params = parse_file(opts, opts.filename)
+        run_part(opts, run, opts.part, opts.filename, params)
+        return 0
+
+    passed = 0
+    failed = 0
+    for part in run.keys():
+        if opts.part and opts.part != part:
+            continue
+        for filename in run[part]["files"].keys():
+            params = parse_file(opts, filename)
+            if run_part(opts, run, part, filename, params):
+                passed += 1
+            else:
+                failed += 1
+    print(f"Test results: {passed} passed, {failed} failed.")
     return 0
 
 if __name__ == "__main__":
